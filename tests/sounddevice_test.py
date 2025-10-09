2114a04cbf1c462f9cca92126796925c43eb8edc
marianoluna <mariano29878@gmail.com>
2025-10-09 17:16:57 -0600

    Update audiotests
    Audio can now be reproduced. mp3 files tested. Should test more
    filetypes

--- tests/sounddevice_test.py
+++ tests/sounddevice_test.py
@@ -1,5 +1,6 @@
 
 import sounddevice as sd
+import soundfile as sf
 import numpy as np
 
 def test_audio():
@@ -20,12 +21,28 @@ def test_audio():
     sd.play(audio, samplerate= sampling_frequency)
     sd.wait()
 
-def get_audio_devices():
+def select_audio_device(kind: str = None) -> int:
     """Print available audio devices"""
+
+    assert kind in (None, "input", "output"), "type must be None, 'input', or 'output'"
+
+    devices = tuple(enumerate(sd.query_devices()))
+    if kind:
+        devices = tuple(filter(lambda d: d[1]['max_' + kind + '_channels'] > 0, devices))
+    
     print("\nAvailable audio devices:")
-    for devide_id, name in enumerate(sd.query_devices()):
+    for devide_id, name in devices:
         print(f"{devide_id}: {name['name']}")
-    print()
+    while True:
+        try:
+            device_id = int(input("Select a device ID: "))
+            if device_id in dict(devices).keys():
+                print(f"Using device ID {device_id}: {sd.query_devices(device_id)['name']}")
+                return device_id
+            else:
+                print("Invalid device ID. Please try again.")
+        except ValueError:
+            print("Please enter a valid integer for the device ID.")
     # Select a device from user input
     # device_id = int(input("Select a device ID: "))
     # print(f"Using device ID {device_id}: {sd.query_devices(device_id)['name']}")
@@ -37,18 +54,10 @@ def test_audio_io():
     On MacOS, you can use BlackHole as a virtual audio device.
     """
     # Select an input device
-    # print("Available input audio devices:")
-    # for device_id, name in enumerate(sd.query_devices(kind= 'input')):
-    #     print(f"{device_id}: {name}")
-    get_audio_devices()
-    input_device_id = int(input("Select an input device ID: "))
+    input_device_id = select_audio_device(kind= 'input')
 
     # Select an output device
-    # print("Avaliable output audio devices:")
-    # for device_id, name in enumerate(sd.query_devices(kind= 'output')):
-    #     print(f"{device_id}: {name}")
-    get_audio_devices()
-    output_device_id = int(input("Select an output device ID: "))
+    output_device_id = select_audio_device(kind= 'output')
 
     samplerate = 44100
     blocksize = 1024
@@ -73,7 +82,47 @@ def test_audio_io():
     except KeyboardInterrupt:
         print("\nStream stopped")
 
+def test_read_audiofile(filename: str):
+    import threading
+
+    event = threading.Event()
+
+    try:
+        data, fs = sf.read(filename, always_2d=False)
+
+        output_device = select_audio_device(kind= 'output')
+
+        current_frame = 0
+
+        def callback(outdata, frames, time, status):
+            if status:
+                print(status)
+            nonlocal current_frame
+            chunksize = min(len(data) - current_frame, frames)
+            outdata[:chunksize] = data[current_frame:current_frame + chunksize]
+            if chunksize < frames:
+                outdata[chunksize:] = 0
+                raise sd.CallbackStop()
+            current_frame += chunksize
+
+        stream = sd.OutputStream(
+            samplerate=fs,
+            device=output_device,
+            channels=data.shape[1],
+            callback=callback,
+            finished_callback=event.set
+        )
+        with stream:
+            event.wait()  # Wait until playback is finished
+    except KeyboardInterrupt:
+        print('\nInterrupted by user')
+    except Exception as e:
+        print(type(e).__name__ + ': ' + str(e))
+
 if __name__ == "__main__":
     # test_audio()
     # get_audio_devices()
-    test_audio_io()
+    # test_audio_io()
+    test_read_audiofile("tests/sample_audios/test_1.mp3")
+    # test_read_audiofile("tests/sample_audios/test_2.mp3")
+    # test_read_audiofile("tests/sample_audios/test_3.mp3")
