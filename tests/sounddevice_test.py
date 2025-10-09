a640590e72eeae97d4d0c761ba38e083d0561bab
marianoluna <mariano29878@gmail.com>
2025-10-08 23:43:35 -0600

    Audio test
    Before working on the bot, I wanted to test if i could redirect audio
    from my own laptop.

--- tests/sounddevice_test.py
+++ tests/sounddevice_test.py
@@ -2,7 +2,7 @@
 import sounddevice as sd
 import numpy as np
 
-def main():
+def test_audio():
     """Generate a 3-second sine wave at 440 Hz"""
 
     # Sampling frequency
@@ -20,5 +20,60 @@ def main():
     sd.play(audio, samplerate= sampling_frequency)
     sd.wait()
 
+def get_audio_devices():
+    """Print available audio devices"""
+    print("\nAvailable audio devices:")
+    for devide_id, name in enumerate(sd.query_devices()):
+        print(f"{devide_id}: {name['name']}")
+    print()
+    # Select a device from user input
+    # device_id = int(input("Select a device ID: "))
+    # print(f"Using device ID {device_id}: {sd.query_devices(device_id)['name']}")
+
+
+def test_audio_io():
+    """
+    Test audio io by routing the input from a device to a selected output device.
+    On MacOS, you can use BlackHole as a virtual audio device.
+    """
+    # Select an input device
+    # print("Available input audio devices:")
+    # for device_id, name in enumerate(sd.query_devices(kind= 'input')):
+    #     print(f"{device_id}: {name}")
+    get_audio_devices()
+    input_device_id = int(input("Select an input device ID: "))
+
+    # Select an output device
+    # print("Avaliable output audio devices:")
+    # for device_id, name in enumerate(sd.query_devices(kind= 'output')):
+    #     print(f"{device_id}: {name}")
+    get_audio_devices()
+    output_device_id = int(input("Select an output device ID: "))
+
+    samplerate = 44100
+    blocksize = 1024
+
+    def callback(indata, outdata, frames, time, status):
+        if status:
+            print(status)
+        outdata[:] = indata  # copia la entrada directamente a la salida
+
+    try:
+        with sd.Stream(
+            samplerate=samplerate,
+            blocksize=blocksize,
+            dtype='float32',
+            channels=2,
+            callback=callback,
+            device=(input_device_id, output_device_id)
+        ):
+            print("Press Ctrl+C to stop the stream")
+            while True:
+                sd.sleep(1000)
+    except KeyboardInterrupt:
+        print("\nStream stopped")
+
 if __name__ == "__main__":
-    main()
+    # test_audio()
+    # get_audio_devices()
+    test_audio_io()
