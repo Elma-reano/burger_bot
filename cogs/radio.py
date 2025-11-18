9e425d1cc9dca7ab86f83f91694ebe9f866ab7ec
Elma-reano <mariano29878@gmail.com>
2025-11-18 14:04:06 -0600

    Radio update
    __get_station_url now returns full info dict
    custom_radio command created

--- cogs/radio.py
+++ cogs/radio.py
@@ -19,17 +19,17 @@ class Radio(commands.Cog, VoicechatBase):
     def __init__(self, bot):
         self.bot = bot
 
-    def __get_station_url(self, station_name: str | None = None, station_id: str | None = None) -> str | None:
+    def __get_station_info(self, station_name: str | None = None, station_id: str | None = None) -> dict | None:
         assert station_name is not None or station_id is not None, "Either station_name or station_id must be provided."
         if station_id is not None:
             for station in catalogue:
                 if station['id'] == station_id:
-                    return station['url']
+                    return station
             return None
         elif station_name is not None:
             for station in catalogue:
                 if station['name'] == station_name:
-                    return station['url']
+                    return station
         return None
     
     def __get_radio_list(self) -> str:
@@ -73,18 +73,13 @@ class Radio(commands.Cog, VoicechatBase):
 
     @commands.command(name="radio", help="Join voice channel and play radio stream. The station id must be provided.")
     async def radio(self, ctx: commands.Context, station_id: str):
-        station_url = self.__get_station_url(station_id=station_id)
-        if station_url is None:
+        station = self.__get_station_info(station_id=station_id)
+        if station is None:
             await ctx.send(f"Station '{station_id}' not found.")
             return
-        await ctx.send(f"Connecting to radio stream: {station_id}")
-        # voice = ctx.voice_client
-
-        # source = discord.FFmpegPCMAudio(url)
-        # voice.play(
-        #     source,
-        #     after=lambda e: print('Player error: %s' % e) if e else None
-        # )
+        station_url = station['url']
+        station_name = station['name']
+        await ctx.send(f"Connecting to radio stream: {station_name}")
         success, message, error = await self.__play_radio(ctx, station_url)
         if success:
             await ctx.send(message)
@@ -95,26 +90,31 @@ class Radio(commands.Cog, VoicechatBase):
     @discord.slash_command(name="radio", description="Join voice channel and play radio stream")
     @discord.option("station", type=str, description="Select a radio station", choices=catalogue_options, required=True)
     async def radio_slash(self, ctx: discord.ApplicationContext, station_name: str):
-        station_url = self.__get_station_url(station_name=station_name)
+        station = self.__get_station_info(station_name=station_name)
+        station_url = station['url'] if station is not None else None
         if station_url is None:
             await ctx.respond(f"Station '{station_name}' not found.")
             return
-        # voice = ctx.voice_client
-        # source = discord.FFmpegAudio(station_url)
-        # voice.play(
-        #     source,
-        #     after=lambda e: print('Player error: %s' % e) if e else None
-        # )
-        # await ctx.respond(f"Playing radio station: {station}")
         success, message, error = await self.__play_radio(ctx, station_url)
         if success:
             await ctx.respond(message)
         else:
             await ctx.respond(f"{message}")
             raise commands.CommandError(error)
+        
+    @commands.command(name="custom_radio", help="Join voice channel and play custom radio stream from URL")
+    async def custom_radio(self, ctx: commands.Context, url: str):
+        await ctx.send(f"Connecting to custom radio stream: {url}")
+        success, message, error = await self.__play_radio(ctx, url)
+        if success:
+            await ctx.send(message)
+        else:
+            await ctx.send(f"{message}")
+            raise commands.CommandError(error)
 
     @radio.before_invoke
     @radio_slash.before_invoke
+    @custom_radio.before_invoke
     async def ensure_voice(self, ctx: discord.ApplicationContext | commands.Context):
         await self._ensure_voice(ctx)
 
